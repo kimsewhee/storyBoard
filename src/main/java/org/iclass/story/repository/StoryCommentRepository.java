c0812c81580f1816b46caa3d99aa3af4e1689f36
kimsewhee <koreait.2021@gmail.com>
2023-02-08 22:21:29 +0900

    #13 - data rest 설정

--- src/main/java/org/iclass/story/repository/StoryCommentRepository.java
+++ src/main/java/org/iclass/story/repository/StoryCommentRepository.java
@@ -3,7 +3,9 @@ package org.iclass.story.repository;
 import org.iclass.story.domain.Story;
 import org.iclass.story.domain.StoryComment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface StoryCommentRepository extends JpaRepository<StoryComment,Long> {
 
 }
--- src/main/java/org/iclass/story/repository/StoryRepository.java
+++ src/main/java/org/iclass/story/repository/StoryRepository.java
@@ -2,7 +2,9 @@ package org.iclass.story.repository;
 
 import org.iclass.story.domain.Story;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface StoryRepository extends JpaRepository<Story,Long> {
 
 }

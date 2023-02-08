a3693949c0f7380b6655d5747eaab34eb2dd5af6
kimsewhee <koreait.2021@gmail.com>
2023-02-08 23:07:24 +0900

    #13_spring data rest 테스트 후 비활성화

--- src/test/java/org/iclass/story/DataRestTest.java
+++ src/test/java/org/iclass/story/DataRestTest.java
@@ -1,5 +1,6 @@
 package org.iclass.story;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +15,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@Disabled("spring data rest 테스트 후 비활성화")
 @DisplayName("Data REST - API 테스트")
 @Transactional
 @AutoConfigureMockMvc

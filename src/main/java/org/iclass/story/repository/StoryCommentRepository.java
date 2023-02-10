c2dbc8cd591c7940e3f0b0456054622d11ffe944
kimsewhee <koreait.2021@gmail.com>
2023-02-10 22:49:03 +0900

    #13_Querydsl 지원을 이용하여 api 검색

--- src/main/java/org/iclass/story/repository/StoryCommentRepository.java
+++ src/main/java/org/iclass/story/repository/StoryCommentRepository.java
@@ -1,11 +1,25 @@
 package org.iclass.story.repository;
 
-import org.iclass.story.domain.Story;
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.StringExpression;
+import org.iclass.story.domain.QStoryComment;
 import org.iclass.story.domain.StoryComment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface StoryCommentRepository extends JpaRepository<StoryComment,Long> {
+public interface StoryCommentRepository
+        extends JpaRepository<StoryComment,Long> ,QuerydslPredicateExecutor<StoryComment>, QuerydslBinderCustomizer<QStoryComment> {
 
+    @Override
+    default void customize(QuerydslBindings bindings, QStoryComment root){
+        bindings.excludeUnlistedProperties(true);
+        bindings.including(root.content,root.createdAt,root.createdBy);
+        bindings.bind(root.content).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.createdAt).first(DateTimeExpression::eq);
+        bindings.bind(root.createdBy).first(StringExpression::containsIgnoreCase);
+    }
 }
--- src/main/java/org/iclass/story/repository/StoryRepository.java
+++ src/main/java/org/iclass/story/repository/StoryRepository.java
@@ -1,10 +1,26 @@
 package org.iclass.story.repository;
 
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.StringExpression;
+import org.iclass.story.domain.QStory;
 import org.iclass.story.domain.Story;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface StoryRepository extends JpaRepository<Story,Long> {
+public interface StoryRepository extends JpaRepository<Story,Long> , QuerydslPredicateExecutor<Story>, QuerydslBinderCustomizer<QStory> {
 
+    @Override
+    default void customize(QuerydslBindings bindings, QStory root){
+        bindings.excludeUnlistedProperties(true);
+        bindings.including(root.title,root.content,root.hashtag,root.createdAt,root.createdBy);
+        bindings.bind(root.title).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.content).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.hashtag).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.createdAt).first(DateTimeExpression::eq);
+        bindings.bind(root.createdBy).first(StringExpression::containsIgnoreCase);
+    }
 }

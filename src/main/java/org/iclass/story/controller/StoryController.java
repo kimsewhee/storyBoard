ceea8f8b3cb61be7a87476a2270c68224b7951a7
kimsewhee <koreait.2021@gmail.com>
2023-02-14 20:41:21 +0900

    #16 뷰 기본 작성

--- src/main/java/org/iclass/story/controller/StoryController.java
+++ src/main/java/org/iclass/story/controller/StoryController.java
@@ -1,9 +1,20 @@
 package org.iclass.story.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @RequestMapping("/stories")
 @Controller
 public class StoryController {
+
+    @GetMapping
+    public String stories(ModelMap map) {
+        map.addAttribute("stories", List.of());
+        return "stories/index";
+    }
+
 }

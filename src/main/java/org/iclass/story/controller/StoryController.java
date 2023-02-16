781bcadb11d31ce3aca8483a3166c9a6363273f6
kimsewhee <koreait.2021@gmail.com>
2023-02-16 19:28:37 +0900

    #16 상세보기 뷰

--- src/main/java/org/iclass/story/controller/StoryController.java
+++ src/main/java/org/iclass/story/controller/StoryController.java
@@ -3,6 +3,7 @@ package org.iclass.story.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -15,6 +16,13 @@ public class StoryController {
     public String stories(ModelMap map) {
         map.addAttribute("stories", List.of());
         return "stories/index";
+    }
+
+    @GetMapping("/{storyId}")
+    public String story(@PathVariable Long storyId, ModelMap map){
+    //    map.addAttribute("story", "story");
+   //     map.addAttribute("storyComments", List.of());
+        return "stories/detail";
     }
 
 }
